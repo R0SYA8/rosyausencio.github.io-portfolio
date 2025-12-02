178676b56ef4db801aa259f3daa7be02b285644c
R0SYA8 <rausencio@ualr.edu>
2025-12-02 06:39:34 +0000

    fixed all the errors

--- assets/css/script.js
+++ assets/css/script.js
@@ -7,3 +7,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+<script>
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+</script>
